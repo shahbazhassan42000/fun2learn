a83dac01ca91350db9c5796bb5a0ebd3ec3e1b6f
Shahbaz Hassan <shahbazhassan42000@gmail.com>
2022-11-09 19:16:51 +0500

    Added application project folders and files

---
+++ app/src/main/java/shahbaz4311/fun2learn/MainActivity.java
@@ -0,0 +1,19 @@
+package shahbaz4311.fun2learn;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.counter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
