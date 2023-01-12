c35729d930375b72cb7ccac63bd8f6368ab77a9d
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2023-01-12 23:55:43 +0500

    creating authentication page

--- app/src/main/java/shahbaz4311/fun2learn/MainActivity.java
+++ app/src/main/java/shahbaz4311/fun2learn/MainActivity.java
@@ -2,8 +2,11 @@ package shahbaz4311.fun2learn;
 
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,11 +25,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView inpName,errorMsg;
-    Button startBtn;
-    String userName;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+
+    TextView username_input, password_input, msg_label;
+    Button login_btn, signup_btn;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +38,101 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
 
-        inpName=findViewById(R.id.inpName);
-        errorMsg=findViewById(R.id.errorMsg);
-        startBtn=findViewById(R.id.startBtn);
+        username_input = findViewById(R.id.username_input);
+        password_input = findViewById(R.id.password_input);
+        msg_label = findViewById(R.id.msg_label);
+        login_btn = findViewById(R.id.login_btn);
+        signup_btn = findViewById(R.id.signup_btn);
 
-        startBtn.setOnClickListener(view -> {
-            String name= String.valueOf(inpName.getText());
-            if(!name.equals("")){
-                userName=name;
-                Intent quizIntent= new Intent(getBaseContext(), quizActivity.class);
-                quizIntent.putExtra("userName",userName);
-                startActivity(quizIntent);
-                finish();
-            }else{
-                errorMsg.setText("Enter your name first to start quiz!!!");
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        errorMsg.setText("");
-                    }
-                },3000);
-            }
-        });
+
+//        inpName=findViewById(R.id.inpName);
+//        errorMsg=findViewById(R.id.errorMsg);
+//        startBtn=findViewById(R.id.startBtn);
+//
+//        startBtn.setOnClickListener(view -> {
+//            String name= String.valueOf(inpName.getText());
+//            if(!name.equals("")){
+//                userName=name;
+//                Intent quizIntent= new Intent(getBaseContext(), quizActivity.class);
+//                quizIntent.putExtra("userName",userName);
+//                startActivity(quizIntent);
+//                finish();
+//            }else{
+//                errorMsg.setText("Enter your name first to start quiz!!!");
+//                new Handler().postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        errorMsg.setText("");
+//                    }
+//                },3000);
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View view) {
+//        switch (view.getId()){
+//            case R.id.login_btn:
+//                String username = username_input.getText().toString();
+//                String password = password_input.getText().toString();
+//                if(username.equals("") || password.equals("")){
+//                    msg_label.setText("Please fill all the fields");
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            msg_label.setText("");
+//                        }
+//                    },3000);
+//                }else{
+//                    user = new User(username,password);
+//                    if(user.login()){
+//                        Intent intent = new Intent(getBaseContext(), quizActivity.class);
+//                        intent.putExtra("user",user);
+//                        startActivity(intent);
+//                        finish();
+//                    }else{
+//                        msg_label.setText("Invalid username or password");
+//                        new Handler().postDelayed(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                msg_label.setText("");
+//                            }
+//                        },3000);
+//                    }
+//                }
+//                break;
+//            case R.id.signup_btn:
+//                Intent intent = new Intent(getBaseContext(), signupActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+        //disable login and signup button if any field is empty
+        if (!username_input.getText().toString().trim().equals("") && !password_input.getText().toString().trim().equals("")) {
+            login_btn.setEnabled(true);
+            signup_btn.setEnabled(true);
+        } else {
+            login_btn.setEnabled(false);
+            signup_btn.setEnabled(false);
+        }
+
+        //change border color to red if password is less than 8 characters
+        password_input.setBackgroundTintList(password_input.getText().toString().trim().length() < 8?ColorStateList.valueOf(getColor(R.color.red)):ColorStateList.valueOf(getColor(R.color.secondary)));
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 }
---
+++ app/src/main/java/shahbaz4311/fun2learn/User.java
@@ -0,0 +1,36 @@
+package shahbaz4311.fun2learn;
+
+import androidx.annotation.NonNull;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "username: "+username+" \tpassword: "+password;
+    }
+
+}
